22986ca05b03ab64ae83dd4cfdacd6b284c51c2e
Zodak <ojepaul4jesus@gmail.com>
2022-12-30 00:22:23 +0100

    chore : ADD A COMPONENT IF NO THERE ARE NO CHATS

---
+++ src/components/Homepage/ChatList/EmptyChats.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import liveChat from "../../../images/svgs/livechat.svg";
+import { motion } from "framer-motion";
+
+const StyledContainer = styled(motion.div)`
+  margin-top: 3rem;
+`;
+const StyledSvgImage = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 30rem;
+  margin: auto;
+  max-height: 30rem;
+`;
+const StyledHeader = styled.div`
+  font-size: 1.7rem;
+  font-family: inherit;
+`;
+const StyledText = styled.p``;
+const EmptyChats = () => {
+  const container = {
+    hidden: {
+      opacity: 0,
+      scale: 0,
+    },
+    show: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+        delay: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+      },
+    },
+  };
+
+  return (
+    <StyledContainer variants={container} initial="hidden" animate="show">
+      <center>
+        <StyledSvgImage src={liveChat} />
+        <StyledHeader>It's nice to chat with someone</StyledHeader>
+        <StyledText>Click the button below to start a chat!</StyledText>
+      </center>
+    </StyledContainer>
+  );
+};
+
+export default EmptyChats;
